070ce1aa8a4097636c7219f347d72d8140a67f48
admareis <reis802@gmail.com>
2026-05-07 19:39:16 -0300

    Novas alterações

---
+++ script.js
@@ -0,0 +1,14 @@
+function altaPressao() {
+  alert("Alta Pressão: Ideal para prédios e sistemas que exigem força total!");
+}
+
+function submersivel() {
+  alert("Submersíveis: Bombas projetadas para trabalhar dentro da água!");
+}
+
+function manutencao() {
+  alert("Manutenção: Suporte técnico especializado para todos os modelos!");
+}
+function enviarMensagem(){
+    alert("Mensagem enviada com sucesso! Entraremos em contato em breve.")
+}
