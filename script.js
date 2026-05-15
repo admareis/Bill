88168fb6967d0e9c9342b3b23f95214794899fa3
admareis <reis802@gmail.com>
2026-05-14 22:10:48 -0300

    tela de sistema interno

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function manutencao() {
 function enviarMensagem(){
     alert("Mensagem enviada com sucesso! Entraremos em contato em breve.")
 }
+
+const emailValido = "admin@admin.com";
+const senhaValida = "admin123";
+
+function fazerLogin(){
+  const email = document.getElementById("campoEmail").value;
+  const senha = document.getElementById("campoSenha").value;
+
+  if (email!==emailValido && senha!==senhaValida){
+    window.alert("Confira ambas as credenciais");
+    return;
+  }
+
+  if(email!==emailValido){
+    window.alert("Confira o seu email");
+    return;
+  }
+  if (senha!==senhaValida){
+    window.alert("Confira sua senha");
+    return;
+  }  
+  window.location.href="sistemainterno.html";  
+}
+function sair() {
+      window.location.href = "index.html";
+    }
